88f68f05e9b04624c8800b0dfae25cdecbd5a10d
Nguyễn Quốc Đại <20020389@vnu.edu.vn>
2023-04-08 23:52:43 +0700

    add usememo util

--- Lib/Util.cs
+++ Lib/Util.cs
@@ -2,5 +2,15 @@ namespace PhoneShop.Lib;
 
 public class Util
 {
-
+  public static async Task<T?> useMemo<T>(Func<Task<T>> callback)
+  {
+    try
+    {
+      return await callback();
+    }
+    catch
+    {
+      return default(T);
+    }
+  }
 }
